24b799f0e6f1c358112c7e9d1a0343c0eb67605c
ntbryant <nicktb@pdx.edu>
2017-07-14 11:35:29 -0700

    changes to front page and favicon

--- blogdown.R
+++ blogdown.R
@@ -1,12 +1,20 @@
 
 library(blogdown)
 
-setwd("D:/Google Drive/GitHub")
+# setwd("D:/Google Drive/GitHub")
 
-new_site(dir = 'blog', # Local repo folder name
-         theme = 'sethmacleod/prologue',
-         format = 'yaml') # 'toml' or 'yaml'
+# new_site(dir = 'blog', # Local repo folder name
+#          theme = 'sethmacleod/prologue',
+#          format = 'yaml') # 'toml' or 'yaml'
+
+# in order to edit the front page I edited the index.html file located in blog/themes/prologue/layouts
+
+# I saved images that are called in blog/static/img
 
 setwd("D:/Google Drive/GitHub/blog")
 serve_site()
 build_site()
+
+
+# Create a new post
+new_post(title = 'your-post.Rmd')
